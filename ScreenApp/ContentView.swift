072b1908a1b3a85d9d80033f71861880d27b3f3b
Jie Ning <jie-ning@live.com>
2026-07-11 10:19:23 +0800

    changed colour

--- ScreenApp/ContentView.swift
+++ ScreenApp/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.blue
+            Color.red
             VStack{
                 Text("Jie Ning")
                     .font(.largeTitle)
